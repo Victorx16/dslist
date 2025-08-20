254e24e564befe921b95e5e332ffd38660ff7e2b
Victorx16 <victorxavi418@gmail.com>
2025-08-20 17:38:53 -0300

    Game repository, service, controller

---
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -0,0 +1,27 @@
+// Controlador REST, é aqui que vai disponibilizar a api.
+
+package com.devsuperior.dslist.controllers;
+
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        var result = gameService.findAll();
+        return result;
+    }
+}
